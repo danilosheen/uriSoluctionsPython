285154987c837604f8681c5fcdfe3c1de93dc277
danilosheen <c.danilo.f.silva@gmail.com>
2024-01-25 14:46:25 -0300

    solution: 1183

---
+++ 1183.py
@@ -0,0 +1,29 @@
+matriz = []
+operacao = input()
+
+for i in range(12):
+  matriz.append([0]*12)
+
+for linha in range(12):
+  for coluna in range(12):
+    matriz[linha][coluna] = (float(input()))
+
+
+ci = 1
+cf = 11
+
+li = 0
+lf = 10
+soma = 0
+
+for l in range(li, lf+1):
+  for c in range(ci, cf+1):
+    soma += matriz[l][c]
+  ci += 1
+
+if operacao == "S":
+  print("{:.1f}".format(soma))
+
+elif operacao == "M":
+  media = soma/66
+  print("{:.1f}".format(media))
